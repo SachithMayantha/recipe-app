63f0c4316bcae82fb13e4b8fe7bde37bd118ae1a
Sachith Mayantha Fernando <38182512+SachithMayantha@users.noreply.github.com>
2020-05-19 15:30:30 +0530

    Number counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  //Authentification
+  const APP_ID = "21a1ba30";
+  const APP_KEY = "e63e2d27d9fd6290ad5d6cd717d72f8a";
+
+  useEffect(() => {
+    
+  },[]);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-form" type="text"/>
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      <h1 onClick={()=>setCounter(counter +1)}>{counter}</h1>
     </div>
   );
 }
