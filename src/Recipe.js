be00c9d43f12ab5eb39331aca7a68dd5db4cf3ea
Sachith Mayantha Fernando <38182512+SachithMayantha@users.noreply.github.com>
2020-05-21 16:15:57 +0530

    basic functions done

--- src/Recipe.js
+++ src/Recipe.js
@@ -4,9 +4,9 @@ import style from './recipe.module.css'
 //parsing data to component
 const Recipe = ({title,calories,image, ingredients}) => {
     return(
-        <div>
-            <h1 className={style.recipe}>{title}</h1>
-            <img src={image} alt=""/>
+        <div className={style.recipe}>
+            <h1>{title}</h1>
+            <img className={style.image} src={image} alt=""/>
             <ol>
                 {ingredients.map(ingredient =>(
                     <li>{ingredient.text}</li>
