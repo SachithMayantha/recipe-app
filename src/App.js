f704c17d38fd0fe5957f297503e32b79d2b5cbe2
Sachith Mayantha Fernando <38182512+SachithMayantha@users.noreply.github.com>
2020-05-21 13:26:02 +0530

    set Search bar for search

--- src/App.js
+++ src/App.js
@@ -8,17 +8,20 @@ function App() {
   const APP_ID = "21a1ba30";
   const APP_KEY = "e63e2d27d9fd6290ad5d6cd717d72f8a"; 
 
-  const [recipes, setRecipes ] = useState([]); 
+  const [recipes, setRecipes ] = useState([]);
+  const [search, setSearch] = useState(""); 
+  //Set query to stop searching when typing
+  const [query, setQuery] = useState('chicken');
 
   //useEffect for to do something after render
   useEffect(() => {
     getRecipes();
-  },[]);
+  },[search]);
 
   //fetch the data
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
       );
       //can use await keyword in async function
       //await for promise to be resolve or rejected
@@ -27,10 +30,22 @@ function App() {
       console.log(data.hits); 
   };
 
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    console.log(search);
+  }
+
+  const getSearch = e => {
+    //stop the page Refresh
+    e.preventDefault();
+    //Search data after the click search button 
+    setQuery(search);
+  }
+
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-form" type="text"/>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-form" type="text" value={search} onChange={updateSearch} />
         <button className="search-button" type="submit">
           Search
         </button>
