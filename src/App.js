fe5e2127f1bf721baa3d175421b51b77b77eee89
Sachith Mayantha Fernando <38182512+SachithMayantha@users.noreply.github.com>
2020-05-21 22:09:26 +0530

    Updated

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, {useState,useEffect} from 'react';
+import Recipe from "./Recipe";
+import './App.css';
+//import banner from "./images/banner.jpg";
+
+function App() {
+
+  //Authentification
+  const APP_ID = "21a1ba30";
+  const APP_KEY = "e63e2d27d9fd6290ad5d6cd717d72f8a"; 
+
+  const [recipes, setRecipes ] = useState([]);
+  const [search, setSearch] = useState(""); 
+  //Set query to stop searching when typing
+  const [query, setQuery] = useState('chicken');
+
+  //useEffect for to do something after render
+  useEffect(() => {
+    getRecipes();
+  },[query]);
+
+  //fetch the data
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+      );
+      //can use await keyword in async function
+      //await for promise to be resolve or rejected
+      const data = await response.json();
+      setRecipes(data.hits); 
+      console.log(data.hits); 
+  };
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = e => {
+    //stop the page Refresh
+    e.preventDefault();
+    //Search data after the click search button 
+    setQuery(search);
+    //Clean search box after display result
+    setSearch('');
+  }
+
+  return (
+    <div className="App">
+      
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="recipes">
+      {recipes.map(recipe =>(
+        //taken data from state, pass to props
+        <Recipe
+        key={recipe.recipe.label}
+        title={recipe.recipe.label}
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients}
+        url={recipe.recipe.url}
+        />
+       ))}
+       </div>
+       <footer><p><b>Owner : Sachith Mayantha Fernando</b></p></footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,20 @@
+import React from "react";
+import style from './recipe.module.css'
+
+//parsing data to component
+const Recipe = ({title,calories,image, ingredients, url}) => {
+    return(
+        <div className={style.recipe}>
+            <h1>{title}</h1>
+            <img className={style.image} src={image} alt=""/>
+            <ol>
+                {ingredients.map(ingredient =>(
+                    <li>{ingredient.text}</li>
+                ))}
+            </ol>
+            <p><b>Calories : </b>{calories}</p>
+            <a className={style.try} href={url}>Try it.!</a>
+        </div>
+    );
+};
+export default Recipe;
