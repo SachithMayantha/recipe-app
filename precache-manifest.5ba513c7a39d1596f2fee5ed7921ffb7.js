6e176c8b53ae92dc76cef1604c9bacbec7f0c79f
Sachith Mayantha Fernando <38182512+SachithMayantha@users.noreply.github.com>
2020-05-21 21:44:42 +0530

    Updates

---
+++ precache-manifest.5ba513c7a39d1596f2fee5ed7921ffb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27962c1694bfb3981c93ca750fc7bfbc",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "1f20e4c8b41b2446d2db",
+    "url": "/recipe-app/static/css/main.c983c971.chunk.css"
+  },
+  {
+    "revision": "801687b449675d4a834e",
+    "url": "/recipe-app/static/js/2.3aa969d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.3aa969d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f20e4c8b41b2446d2db",
+    "url": "/recipe-app/static/js/main.ba0cb211.chunk.js"
+  },
+  {
+    "revision": "7ff5b27d3f9efd2f4352",
+    "url": "/recipe-app/static/js/runtime-main.96283c75.js"
+  },
+  {
+    "revision": "8998927f70834301c79ee2bdfa5c56e3",
+    "url": "/recipe-app/static/media/banner.8998927f.jpg"
+  }
+]);
