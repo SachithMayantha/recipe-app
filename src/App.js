101e57a48adf38db0ef19ea6ddcada6c51034f75
Sachith Mayantha Fernando <38182512+SachithMayantha@users.noreply.github.com>
2020-05-21 07:24:55 +0530

    used useEffect

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ function App() {
 
   //Authentification
   const APP_ID = "21a1ba30";
-  const APP_KEY = "e63e2d27d9fd6290ad5d6cd717d72f8a";
+  const APP_KEY = "e63e2d27d9fd6290ad5d6cd717d72f8a"; 
+
+  const [recipes, setRecipes ] = useState([]); 
 
   useEffect(() => {
-    
+    getRecipes();
   },[]);
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
-  }
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      );
+      const data = await response.json();
+      console.log(data.hits);
+  };
 
   return (
     <div className="App">
@@ -23,7 +29,6 @@ function App() {
           Search
         </button>
       </form>
-      <h1 onClick={()=>setCounter(counter +1)}>{counter}</h1>
     </div>
   );
 }
